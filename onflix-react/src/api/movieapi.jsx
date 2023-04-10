68f5cf065063113613190dfb97b18fa1eddb757b
Ilhamsyah Putra <ilhamsyah.putraa1997@gmail.com>
2023-04-11 01:25:49 +0700

    masih error di modal 2

--- onflix-react/src/api/movieapi.jsx
+++ onflix-react/src/api/movieapi.jsx
@@ -3,50 +3,53 @@ import axios from "axios";
 const apiKey = import.meta.env.REACT_APP_APIKEY;
 const baseURL = "https://api.themoviedb.org/3";
 
-export const getMovieDetails = async (movieIds) => {
-  const popularMovies = await axios.get(`${baseURL}/movie/popular`, {
-    params: {
-      api_key: apiKey,
-    },
-  });
+export const getMovieDetails = async (movieId) => {
+  try {
+    const popularMovies = await axios.get(`${baseURL}/movie/popular`, {
+      params: {
+        api_key: apiKey,
+      },
+    });
 
-  const movies = await Promise.all(
-    popularMovies.data.results.map(async (movie) => {
-      const { data } = await axios.get(`${baseURL}/movie/${movie.id}`, {
-        params: {
-          api_key: apiKey,
-          append_to_response: "videos,credits",
-        },
-      });
+    const movies = await Promise.all(
+      popularMovies.data.results.map(async (movie) => {
+        const { data } = await axios.get(`${baseURL}/movie/${movie.id}`, {
+          params: {
+            api_key: apiKey,
+            append_to_response: "videos,credits",
+          },
+        });
 
-      const director = data.credits.crew.find((crew) => crew.job === "Director");
-      const trailer = data.videos.results.find((video) => video.type === "Trailer");
-      const cast = data.credits.cast.slice(0, 4).map((castMember) => ({
-        name: castMember.name,
-        character: castMember.character,
-        profilePicture: castMember.profile_path
-          ? `https://image.tmdb.org/t/p/w500${castMember.profile_path}`
-          : null,
-      }));
+        const director = data.credits.crew.find((crew) => crew.job === "Director");
+        const trailer = data.videos.results.find((video) => video.type === "Trailer");
+        const cast = data.credits.cast.slice(0, 4).map(({ name, character, profile_path }) => ({
+          name,
+          character,
+          profilePicture: profile_path ? `https://image.tmdb.org/t/p/w500${profile_path}` : null,
+        }));
 
-      return {
-        id: data.id,
-        title: data.title,
-        poster: `https://image.tmdb.org/t/p/w500${data.poster_path}`,
-        backdrop: `https://image.tmdb.org/t/p/w500${data.backdrop_path}`,
-        runtime : data.runtime,
-        overview: data.overview,
-        rating: data.vote_average,
-        genres: data.genres.map((genre) => genre.name).join(", "),
-        director: {
-          name: director ? director.name : "",
-          profilePicture: director && director.profile_path ? `https://image.tmdb.org/t/p/w500${director.profile_path}` : null,
-        },
-        trailer: trailer ? `https://www.youtube.com/watch?v=${trailer.key}` : "",
-        cast,
-      };
-    })
-  );
+        return {
+          id: data.id,
+          title: data.title,
+          poster: `https://image.tmdb.org/t/p/w500${data.poster_path}`,
+          backdrop: `https://image.tmdb.org/t/p/w500${data.backdrop_path}`,
+          runtime: data.runtime,
+          overview: data.overview,
+          release_date: data.release_date,
+          rating: data.vote_average,
+          genres: data.genres.map((genre) => genre.name).join(", "),
+          director: {
+            name: director ? director.name : "",
+            profilePicture: director && director.profile_path ? `https://image.tmdb.org/t/p/w500${director.profile_path}` : null,
+          },
+          trailer: trailer ? `https://www.youtube.com/watch?v=${trailer.key}` : "",
+          cast,
+        };
+      })
+    );
 
-  return movies;
+    return movies;
+  } catch (error) {
+    console.log(error);
+  }
 };
--- onflix-react/src/components/MovieCombine.jsx
+++ onflix-react/src/components/MovieCombine.jsx
@@ -6,9 +6,11 @@ import PlayButton from '../assets/play-fill 1.svg'
 import { getMovieDetails } from '../api/movieapi';
 
 
-const MovieCombine = () => {
+function MovieCombine() {
 
     const [movies, setMovie] = useState([]);
+
+    
 
     useEffect(() => {
         async function fetchMovies() {
@@ -20,32 +22,35 @@ const MovieCombine = () => {
     }, []);
 
 
-    const PopularMovieList = () => {
-        {movies.map((movie) => (
-            
+
+
+    return (
+        <>
+        <div className="column d-flex justify-content-start col-12  position-relative me-3 ms-3 product-card-container">
+            {movies.map((movie) => (
             <div className='cardAndModal'>
                 {/* Card */}
                 <div className="card bg-dark text-white col-lg-2 col-md-3 col-sm-4 col-4" key={movie.id}>
-                    <img src={`https://image.tmdb.org/t/p/w500/${movie.poster}`} className="card-img" alt="..." />
+                    <img src={`https://image.tmdb.org/t/p/w500/${movie.poster}`} className="card-img" alt="{movie.title}" />
                     <div className="card-img-overlay d-flex align-items-end">
-                        <div className="rate-label position-absolute end-0"><img src={RateIcon} alt="" /> {movie.vote_average}</div>
+                        <div className="rate-label position-absolute end-0"><img src={RateIcon} alt={movie.title} /> {movie.vote_average}</div>
                         <div className="text-container position-absolute" >
                             <div className="info-icons">
                                 <span className="info-label">
-                                    <img src={TimeIcon} alt="" />{movie.runtime} Minutes</span>
+                                    <img src={TimeIcon} alt={movie.title} />{movie.runtime} Minutes</span>
                                 <span className="info-label">
-                                    <img src={ReleaseIcon} alt="" />{movie.release_date}</span>
+                                    <img src={ReleaseIcon} alt={movie.title} />{movie.release_date}</span>
                             </div>
                             <h5 className="card-title col-12">{movie.title}</h5>
                             <span className='ButtonWrapper col-12'>
-                                <button type="button" className="btn btn-warning btn-view col-5" data-bs-toggle="modal" data-bs-target="#exampleModal3">View</button>
-                                <a className="btn btn-secondary btn-watch col-5" href={movie.trailer} role="button" target="blank"><img src={PlayButton} alt="" className="watch-icon" /> Watch</a>
+                                <button type="button" className="btn btn-warning btn-view col-5" data-toggle="modal" data-bs-target="#movieModal">View</button>
+                                <a className="btn btn-secondary btn-watch col-5" href={movie.trailer} role="button" target="blank"><img src={PlayButton} alt={movie.title} className="watch-icon" /> Watch</a>
                             </span>
                         </div>
                     </div>
                 </div>
                 {/* Modal */}
-                <div className="modal modal-lg fade" id="exampleModal3" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal modal-lg fade" id="movieModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                     <div className="modal-dialog">
                     <div className="modal-content">
                         <div className="modal-body">
@@ -55,31 +60,31 @@ const MovieCombine = () => {
                             <div className="landscapePosterOverlay"><button type="button" className="btn-close end-0" data-bs-dismiss="modal" aria-label="Close"></button>
                             </div>
                             <div className="container-fluid d-flex container-landscape-poster">
-                                <img src= {movie.backdrop} alt="" className="landscape-poster"/>
+                                <img src= {movie.backdrop} alt={movie.title} className="landscape-poster"/>
                             </div>
                         </div>
                         <div className="container-fluid d-flex modal-content-container">
                             <div id="modal-content-container" className="container-fluid d-flex">
                                 {/* Container 1 : Poster */}
                             <div className="col-5 modal-regular-poster-container">
-                                <img src={movie.poster} alt="" className="regular-poster-modal " style="border-radius: 1.5rem;"/>
+                                <img src={movie.poster} alt={movie.title} className="regular-poster-modal " style={{ borderRadius: '1.5rem' }}/>
                             </div>
                             {/* Container 2 : Description */}
                             <div className="col-7 modal-description">
-                                <img src={movie.poster} alt="" className="regular-poster-modal " style="border-radius: 1.5rem;"/>
+                                <img src={movie.poster} alt={movie.title} className="regular-poster-modal " style={{ borderRadius: '1.5rem' }}/>
                                 <h1 id="modal-title">{movie.title}</h1>
                                 <span >
-                                    <span className="info-label-modal"> <img src="src/clock.svg" alt=""/>{movie.runtime} Minutes</span>
-                                    <span className="info-label-modal"> <img src="src/eye.svg" alt=""/>{movie.release_date}</span>
+                                    <span className="info-label-modal"> <img src="src/clock.svg" alt={movie.title}/>{movie.runtime} Minutes</span>
+                                    <span className="info-label-modal"> <img src="src/eye.svg" alt={movie.title}/>{movie.release_date}</span>
                                 </span>
                                 <span>
                                     <span>
                                         <a className="btn btn-secondary col-6 btn-view-modal" href={movie.trailer} 
-                                        role="button" target="blank"><img src="./src/play-fill 1.svg" alt="" className="watch-icon" /> Watch</a>
+                                        role="button" target="blank"><img src="./src/play-fill 1.svg" alt={movie.title} className="watch-icon" /> Watch</a>
                                     </span>
                                     
-                                    <button type="button" className="btn btn-secondary btn-stroke"> <img src="./src/heart 1.svg" alt="" className="watch-icon"/></button>
-                                    <button type="button" className="btn btn-secondary btn-stroke"> <img src="./src/share-fill 1.svg" alt="" className="watch-icon"/></button>
+                                    <button type="button" className="btn btn-secondary btn-stroke"> <img src="./src/heart 1.svg" alt={movie.title} className="watch-icon"/></button>
+                                    <button type="button" className="btn btn-secondary btn-stroke"> <img src="./src/share-fill 1.svg" alt={movie.title} className="watch-icon"/></button>
 
                                 </span>
                                 <p>{movie.overview}</p>
@@ -95,7 +100,7 @@ const MovieCombine = () => {
                                         <div className="actor-profile-container d-flex row col-2
                                         ">
                                             <div>
-                                                <img src={movie.director.profilePicture} alt="" className="actor-profile"/>
+                                                <img src={movie.director.profilePicture} alt={movie.title} className="actor-profile"/>
                                             </div>
                                             <div>
                                                 <h6  className="actor-name-soon">{movie.director.name}</h6>
@@ -118,15 +123,6 @@ const MovieCombine = () => {
                 </div>
             </div>
         ))}
-    }
-
-
-
-    return (
-        <>
-            
-        <div className="column d-flex justify-content-start col-12  position-relative me-3 ms-3 product-card-container">
-            <PopularMovieList />
         </div>
         
         </>
